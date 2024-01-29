7c11f3213b734d2477aad82120e094a887fcb9c1
anjali803 <anjaliprathees.nv@gmail.com>
2024-01-29 18:13:28 +0530

    signup page created

---
+++ Frontend/pneumoai/src/Pages/SignupPage.jsx
@@ -0,0 +1,54 @@
+import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function SignupPage() {
+  return (
+    <>
+      <div className="login-container">
+        <h1 className="PneumoAI-text">PneumoAI</h1>
+        <p className="login-text">Create new account</p>
+        <div className="button">
+          <button className="facebook-icon-button">
+            <FaFacebook /> Facebook
+          </button>
+          <button className="google-icon-button">
+            <FaGoogle /> Google
+          </button>
+        </div>
+        <form>
+          <div>
+            <input
+              type="text"
+              id="text"
+              name="Name"
+              placeholder="Enter your Name"
+            ></input>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+            ></input>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            ></input>
+          </div>
+        </form>
+
+        <button className="login-button">Login</button>
+
+        <div className="create-account-div">
+          <p>Already member? </p>
+          <Link to="/LoginPage" className="create-account-text">
+            Log in
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SignupPage;
